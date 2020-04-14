09b58233e55fa1380b468379df4cd21716d6f21b
patsO <o.dutchak@tenantcloud.com>
2020-04-14 20:02:49 +0300

    Add protractor custom looger

---
+++ built/clipboard.js
@@ -0,0 +1,33 @@
+class Clipboard {
+	constructor() {
+		this.countDots = 0;
+		this.activeSpecs = null;
+	}
+
+	getCount() {
+		return this.countDots;
+	}
+
+	increaseSpecsCount() {
+		this.countDots += 1;
+	}
+
+	setActiveSpecs(allSpecs) {
+		this.activeSpecs = allSpecs;
+	}
+
+	getActiveSpecs() {
+		return this.activeSpecs;
+	}
+
+	decreaseSpecsCount(countActiveTasks) {
+		if(countActiveTasks !== 0){
+			process.stdout.write('.');
+		}
+		this.activeSpecs = this.activeSpecs - 1;
+	}
+}
+
+let clipboard = new Clipboard();
+
+export { clipboard as Clipboard };
